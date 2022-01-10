f397ce39e3924816704fdb261075f6e03647a446
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-01-10 16:56:47 +0100

    Acquisto utente con carta livello 3

---
+++ classes/utente.php
@@ -0,0 +1,49 @@
+<?php 
+// PARENT CLASS
+
+class User {
+    // Propietà
+    protected $nomeAcquirente;
+    protected $prodotto;
+    protected $prezzo;
+    protected $livelloCarta;
+   
+
+    // COSTRUTTORE 
+    public function __construct($nomeAcquirente, $prodotto, $prezzo, $livelloCarta = 3)
+    {
+        $this->nomeAcquirente = $nomeAcquirente;
+        $this->prodotto = $prodotto;
+        $this->prezzo = $prezzo;
+        $this->livelloCarta = $livelloCarta;
+    }
+
+    public function getNome() {
+        return $this->nomeAcquirente;
+    }
+    public function getProdotto() {
+        return $this->prodotto;
+    }
+
+    public function getPrezzo() {
+        return $this->prezzo;
+    }
+
+
+     protected function SetLivelloCarta() {
+     $this->prezzo =  $this->livelloCarta = 3 ? 250 : 0;
+    }
+
+    public function GetLivelloCarta() {
+
+        // Livello per ricervere lo sconto
+        $this->SetLivelloCarta();
+
+        // ritorno valore
+         return $this->livelloCarta;
+
+    } 
+
+
+    
+}
--- php-oop-shop.php
+++ php-oop-shop.php
@@ -1,3 +1,7 @@
+<?php 
+require_once __DIR__ . '/classes/utente.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,5 +14,25 @@
     <header>
         <h1>PHP OOP SHOP </h1>
     </header>
+
+    <main>
+         <section>
+             <h2>Prodotto in vendita </h2>       
+             <div> Xbox serie s </div>
+             <div>il suo prezzo di listino è 300€</div>
+         </section>
+
+        <section>
+            <?php // istanza
+            $user1 = new User('Alessandro Vanoli', 'Xbox Serie S', 300);
+           /*  var_dump($user1); */
+            ?>
+            <hr>
+            <h5> Nome utente dell'avvenuto acquisto:  <?php echo $user1->getNome(); ?> </h5> 
+            
+            <h5> hai acquistato il prodotto: <?php echo $user1->getProdotto(); ?> <h5>
+            <h5>Grazie alla tua carta livello 3 il tuo costo è di: <?php echo $user1->GetLivelloCarta(); ?>€ <h5>
+        </section>
+    </main>
 </body>
 </html>
